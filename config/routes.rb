79609e38adeae5c09c31008f578fcc271f93e803
EC2 Default User <ec2-user@ip-172-31-82-62.ec2.internal>
2019-12-08 14:14:31 +0000

    final adjust

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  get '/index', to: "users#index"
-
   root 'sessions#new'
-  
+  get '/index', to: "users#index"
   resources :users, only: [:new, :create]
   get "/search", to: "static_pages#search"
   get "/result", to: "static_pages#result"
   get '/incomedetail', to: 'static_pages#incomedetail'
-
   get '/outgodetail', to: 'static_pages#outgodetail'
-  
   resources :incomes, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :outgos, only: [:new, :create, :edit, :update, :destroy]
   get "/totalincome", to: "incomes#home"
   get "/totaloutgo", to: "outgos#home"
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
   get "total", to: "static_pages#total"
   resources :password_resets,     only: [:new, :create, :edit, :update]
 end
